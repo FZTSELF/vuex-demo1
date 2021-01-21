00e92eca818b58ff155c6d19cc21b59fd78053e9
fzt <2630036958@qq.com>
2021-01-21 20:50:28 +0800

    vuex-demo1

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,8 @@
     <button :disabled="num>0?false:true" @click="$store.commit('increment')">购买1个</button>
     <button :disabled="num>1?false:true" @click="$store.commit('increment2')">购买2个 </button><br>
 
-    <button id="btn" :disabled="num>0?false:true" @click="$store.commit('increment3')">购买</button>
-    <input type="number"  min="1" v-model="$store.state.buy">个
+    <button id="btn" :disabled="num>=buyNum?false:true" @click="$store.commit('increment3')">购买</button>
+    <input type="number"  min="1" v-model="$store.state.buyNum" >个
 <!--    {{count}}-->
 <!--    {{$store.getters.doubleCount}}-->
 <!--    <button @click="$store.commit('increment')">count++</button>-->
@@ -26,6 +26,9 @@
     computed: {
       num(){
         return this.$store.state.num
+      },
+      buyNum(){
+        return this.$store.state.buyNum
       }
     }
   }
